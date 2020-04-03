73c4b0cee13131cd493f2c486d7575cebb214565
konuralpt <konuralptoksoy4@gmail.com>
2020-04-04 01:21:51 +0300

    turned to nodeJS app

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+app.use(express.static(__dirname)); //__dir and not _dir
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname+'/rocketLeague/index.html'));
+});
+
+app.get('/rl',function(req,res) {
+  res.sendFile(path.join(__dirname+'/rocketLeague/index.html'));
+});
+
+app.get('/space',function(req,res) {
+  res.sendFile(path.join(__dirname+'/space/index.html'));
+});
+
+var port = 8000; // you can use any port
+app.listen(port);
+console.log('server on' + port);
--- index.php
+++
@@ -1,1 +0,0 @@
-<?php include_once('./rocketLeague/index.html'); ?>
